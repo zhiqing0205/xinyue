e693bef1ccfc72914306681b6ab1f02964e2b79e
zhiqing0205 <1775840762@qq.com>
2025-05-02 23:44:09 +0800

    feat: 添加应用头部配置，包括标题和元数据
    在 nuxt.config.ts 中添加了应用头部的配置，包括页面标题、字符集、视口设置以及网站图标。这些更改旨在提升用户体验和页面的可访问性。

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,17 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/content', '@nuxt/image', '@nuxt/scripts']
+  modules: ['@nuxt/content', '@nuxt/image', '@nuxt/scripts'],
+  app: {
+    head: {
+      title: '欣玥生日快乐',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      ]
+    }
+  }
 })
